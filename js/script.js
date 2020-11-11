fbab57549a3d65253268bc9ef5f97fdcc37e9c99
Igor Trifonov <Ha1lowe3n@yandex.ru>
2020-11-11 19:28:31 +0800

    feat(script.js): added rest operator to class MenuCard

--- js/script.js
+++ js/script.js
@@ -41,7 +41,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
   // Timer 
-  const deadline = '2020-11-11';
+  const deadline = '2020-11-13';
 
   // Функция, которая расчитывает время между дедлайном и настоящим временем
   const getTimeRemaining = (endtime) => {
@@ -61,7 +61,7 @@ document.addEventListener('DOMContentLoaded', () => {
   };
 
   const getZero = (num) => num >= 0 && num < 10 ?
-     `0${num}` : num;
+    `0${num}` : num;
 
   const setClock = (selector, endtime) => {
     const timer = document.querySelector(selector),
@@ -128,7 +128,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
-  //const modalTimerId = setTimeout(openModal, 15000);
+  const modalTimerId = setTimeout(openModal, 15000);
 
   const showModalByScroll = () => {
     if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
@@ -142,13 +142,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Используем классы для карточек
   class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector) {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
       this.src = src;
       this.alt = alt;
       this.title = title;
       this.descr = descr;
       this.price = price;
       this.transfer = 27;
+      this.classes = classes;
       this.parentSelector = document.querySelector(parentSelector);
 
       this.changeToUAH();
@@ -160,16 +161,22 @@ document.addEventListener('DOMContentLoaded', () => {
 
     render() {
       const element = document.createElement('div');
+
+      if (this.classes.length === 0) {
+        this.classes = 'menu__item';
+        element.classList.add(this.classes);
+      } else {
+        this.classes.forEach(className => element.classList.add(className));
+      }
+
       element.innerHTML = `
-        <div class="menu__item">
-          <img src=${this.src} alt=${this.alt}>
-          <h3 class="menu__item-subtitle">${this.title}</h3>
-          <div class="menu__item-descr">${this.descr}</div>
-          <div class="menu__item-divider"></div>
-          <div class="menu__item-price">
-            <div class="menu__item-cost">Цена:</div>
-            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-          </div>
+        <img src=${this.src} alt=${this.alt}>
+        <h3 class="menu__item-subtitle">${this.title}</h3>
+        <div class="menu__item-descr">${this.descr}</div>
+        <div class="menu__item-divider"></div>
+        <div class="menu__item-price">
+          <div class="menu__item-cost">Цена:</div>
+          <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
         </div>
       `;
 
@@ -192,7 +199,8 @@ document.addEventListener('DOMContentLoaded', () => {
     'Меню "Постное"',
     'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
     14,
-    ".menu .container"
+    ".menu .container",
+    'menu__item'
   ).render();
 
   new MenuCard(
@@ -201,7 +209,8 @@ document.addEventListener('DOMContentLoaded', () => {
     'Меню “Премиум”',
     'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
     21,
-    ".menu .container"
+    ".menu .container",
+    'menu__item'
   ).render();
 
 });
