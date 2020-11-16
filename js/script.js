7616a0250ca198e5e8ab374c633f631c13c7a06d
Igor Trifonov <Ha1lowe3n@yandex.ru>
2020-11-16 20:33:56 +0800

    feat(script.js): added posts json from forms

--- js/script.js
+++ js/script.js
@@ -213,4 +213,56 @@ document.addEventListener('DOMContentLoaded', () => {
     'menu__item'
   ).render();
 
+
+  // Forms
+  const forms = document.querySelectorAll('form');
+
+  const message = {
+    loading: 'Загрузка...',
+    success: 'Спасибо! Скоро мы с вами свяжемся',
+    failure: 'Что-то пошло не так...'
+  };
+
+  const postData = (form) => {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      const statusMessage = document.createElement('div');
+      statusMessage.classList.add('status');
+      statusMessage.textContent = message.loading;
+      form.append(statusMessage);
+
+      const request = new XMLHttpRequest();
+      request.open('POST', 'server.php');
+
+      //  Заголовок не нужен при XMLHttpRequest + FormData, он назначается автоматически
+      request.setRequestHeader('Content-type', 'application/json');
+      const formData = new FormData(form);
+
+      const obj = {};
+      formData.forEach((value, key) => obj[key] = value);
+
+      request.send(JSON.stringify(obj));
+
+      request.addEventListener('load', () => {
+        if (request.status === 200) {
+          console.log(request.response);
+          statusMessage.textContent = message.success;
+
+          form.reset();
+          setTimeout(() => {
+            statusMessage.remove();
+          }, 3000);
+        } else {
+          statusMessage.textContent = message.failure;
+        }
+      });
+    });
+  };
+
+  forms.forEach(item => {
+    postData(item);
+  });
+
+
 });
