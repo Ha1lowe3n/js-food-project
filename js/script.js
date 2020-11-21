2ca958452aa22e7652cef622a50078814fabed4e
Igor Trifonov <Ha1lowe3n@yandex.ru>
2020-11-22 04:47:07 +0800

    feat(script.js): test axios

--- js/script.js
+++ js/script.js
@@ -191,35 +191,19 @@ document.addEventListener('DOMContentLoaded', () => {
     return await res.json();
   };
 
-  getResourse('http://localhost:3000/menu')
+  // getResourse('http://localhost:3000/menu')
+  // .then(data => {
+  //   data.forEach(({img, altimg, title, descr, price}) => {
+  //     new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+  //   });
+  // });
+
+  axios.get('http://localhost:3000/menu')
   .then(data => {
-    data.forEach(({img, altimg, title, descr, price}) => {
+    data.data.forEach(({img, altimg, title, descr, price}) => {
       new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
     });
   });
-
-  // const createCard = (data) => {
-  //   data.forEach(({img, altimg, title, descr, price}) => {
-  //     const element = document.createElement('div');
-  //     element.classList.add('menu__item');
-
-  //     element.innerHTML = `
-  //       <img src=${img} alt=${altimg}>
-  //       <h3 class="menu__item-subtitle">${title}</h3>
-  //       <div class="menu__item-descr">${descr}</div>
-  //       <div class="menu__item-divider"></div>
-  //       <div class="menu__item-price">
-  //         <div class="menu__item-cost">Цена:</div>
-  //         <div class="menu__item-total"><span>${price}</span> грн/день</div>
-  //       </div>
-  //     `;
-
-  //     document.querySelector('.menu .container').append(element);
-  //   });
-  // };
-
-  // getResourse('http://localhost:3000/menu')
-  // .then(data => createCard(data));
 
   // Forms
   const forms = document.querySelectorAll('form');
